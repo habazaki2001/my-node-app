a5860aa9d9a409edf9150ae819e7ceb885782d37
habazaki2001 <85531602+habazaki2001@users.noreply.github.com>
2025-02-08 12:08:09 +0700

    test2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Hello from Railway!");
-});
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
